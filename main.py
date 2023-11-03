6eecfd627b85384232df55f371c54e9cf71bf369
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-11-03 18:40:14 +0200

    task 2 ready

--- main.py
+++ main.py
@@ -10,3 +10,17 @@
 #         return True
 #     else:
 #         return False
+#
+# task2: валідація; мобільний номер телефону (тільки цифри, можлива наявність плюса, довжина номера)
+# import re
+#
+#
+# def validate_mobile_phone(phone_number):
+#
+#     pattern = r'^\+?\d{10,12}$'
+#
+#     if re.match(pattern, phone_number):
+#         return True
+#     else:
+#         return False
+#
